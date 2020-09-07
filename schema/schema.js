72bfef59646bc373777753befacaeef14c982d0a
andrewphilippov <a@fandb.pro>
2020-09-07 09:12:20 +0300

    Adding extra data

---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+} = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '33', firstName: 'Andrew', age: 33 }
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`).then(({ data }) => data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+})
